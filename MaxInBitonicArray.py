1f14c97bce3d6adc8a5634441d539b623b5af4e7
AdarshChaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-22 07:12:30 +0100

    Update MaxInBitonicArray.py

--- MaxInBitonicArray.py
+++ MaxInBitonicArray.py
@@ -3,7 +3,7 @@ def findMax(arr):
     high=len(arr)-1
     while(low<=high):
         mid=(low+high)//2
-        if (mid-1)>=0 and arr[mid]>arr[mid-1] and (mid+1)<=len(arr) and arr[mid]>arr[mid+1]:
+        if (mid-1)>=0 and arr[mid]>arr[mid-1] and (mid+1)<len(arr) and arr[mid]>arr[mid+1]:
             return arr[mid]
         elif (mid-1)>= 0 and arr[mid-1]>arr[mid]:
             high=mid-1
